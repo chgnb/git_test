45d333ec344772b8171812e8dd2c66d01e04082a
tom <1151619368@qq.com>
2020-12-21 22:22:58 +0800

    update for loop!

--- src/com/zx/git.java
+++ src/com/zx/git.java
@@ -2,10 +2,8 @@ package com.zx;
 
 public class git {
     public static void main(String[] args) {
-        System.out.println("hello Git_idea!");
-    }
-    public void test(){
-        int i = 0;
-        i+=10;
+        for (int i = 0;1<=10;i++){
+            System.out.println("this is:"+i);
+        }
     }
 }
