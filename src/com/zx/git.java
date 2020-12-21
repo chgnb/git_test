bcda7b186257f1f937f6f5655dff4edf14a83ea9
tom <1151619368@qq.com>
2020-12-21 22:26:57 +0800

    david version

--- src/com/zx/git.java
+++ src/com/zx/git.java
@@ -2,8 +2,10 @@ package com.zx;
 
 public class git {
     public static void main(String[] args) {
-        for (int i = 0;1<=10;i++){
-            System.out.println("this is:"+i);
+        int i = 10;
+        while(i>0){
+            System.out.println(i);
+            i--;
         }
     }
 }
