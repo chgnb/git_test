2014dd2d338db9cf9ee070ba4e7992999edc5745
tom <1151619368@qq.com>
2020-12-21 22:11:01 +0800

    231321 add a test function

--- src/com/zx/git.java
+++ src/com/zx/git.java
@@ -4,4 +4,8 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello Git_idea!");
     }
+    public void test(){
+        int i = 0;
+        i+=10;
+    }
 }
